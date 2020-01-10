56b39b9d7467abb24d626d701269891b6a41bfd7
pal_user <dp.automata@gmail.com>
2020-01-10 14:24:24 -0700

    Eureka

--- exercise_all_endpoints.sh
+++ exercise_all_endpoints.sh
@@ -1,6 +1,14 @@
+function trim_ws {
+    sed 's/^[[:space:]]*//' <<< "$1"
+}
+
 function exercise_registrations {
-    curl -i -XPOST -H"Content-Type: application/json" registration-pal-badname.apps.evans.pal.pivotal.io/registration -d'{"name": "$2"}'
-    curl -i registration-pal-badname.apps.evans.pal.pivotal.io/users/$1
+    POST_RESULT=$(curl -s -XPOST -H"Content-Type: application/json" registration-pal-badname.apps.evans.pal.pivotal.io/registration -d"{\"name\": \"$1\"}" | jq .)
+    echo -e "\n\nPOST"
+    echo $POST_RESULT | jq .
+    GET_RESULT=$(curl -s registration-pal-badname.apps.evans.pal.pivotal.io/users/$(echo $POST_RESULT | jq '.id'))
+    echo -e "\n\nGET"
+    echo $GET_RESULT | jq .
 }
 
 function exercise_projects {
@@ -21,6 +29,42 @@ function exercise_stories {
 function exercise_time_entries {
     curl -i -XPOST -H"Content-Type: application/json" timesheets-pal-badname.apps.evans.pal.pivotal.io/time-entries/ -d"{\"projectId\": $1, \"userId\": $2, \"date\": \"2015-05-17\", \"hours\": 6}"
     curl -i timesheets-pal-badname.apps.evans.pal.pivotal.io/time-entries?userId=$2
+}
+
+function unbind_and_delete {
+    cf delete-service-key -f $2 flyway-migration-key
+    sleep 10s
+    cf unbind-service $1 $2
+    sleep 10s
+    cf delete-service -f $2
+    sleep 10s
+}
+
+function create_dbs {
+    cf create-service p.mysql db-small $1
+}
+
+function cleanup_databases {
+    unbind_and_delete tracker-allocations tracker-allocations-database
+    sleep 10s
+    unbind_and_delete tracker-backlog tracker-backlog-database
+    sleep 10s
+    unbind_and_delete tracker-timesheets tracker-timesheets-database
+    sleep 10s
+    unbind_and_delete tracker-registration tracker-registration-database
+    sleep 10s
+}
+
+function rebuild_databases {
+    create_dbs {tracker-allocations,tracker-backlog,tracker-timesheets,tracker-registration}-database
+}
+
+function bad_travis {
+    ./gradlew clean build
+    cleanup_databases
+    rebuild_databases
+    sleep 10m
+    cf push -f manifest-{registration,allocations,backlog,timesheets}.yml
 }
 
 function submit_multiproject {
